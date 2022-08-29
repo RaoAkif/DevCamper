f6a610225c9e3e158b7f932863d472367403ddc6
raoakif <akifrao@gmail.com>
2022-08-30 02:45:40 +0500

    Add separate controllers folder and file to separate the logic

---
+++ controllers/bootcamps.js
@@ -0,0 +1,34 @@
+// Desc   Get all bootcamps
+// Route  GET /api/v1/bootcamps
+// Acces  Public
+exports.getBootcamps = (req, res, next) => {
+  res.status(200).json({ success: true, msg: "Show all bootcamps" });
+}
+
+// Desc   Get single bootcamp
+// Route  GET /api/v1/bootcamps/:id
+// Acces  Public
+exports.getBootcamp = (req, res, next) => {
+  res.status(200).json({ success: true, msg: `Show bootcamp ${req.params.id}` });
+}
+
+// Desc   Create a bootcamp
+// Route  PUT /api/v1/bootcamps/:id
+// Acces  Public
+exports.createBootcamp = (req, res, next) => {
+  res.status(200).json({ success: true, msg: "Create new bootcamp" });
+}
+
+// Desc   Update single bootcamp
+// Route  GET /api/v1/bootcamps/:id
+// Acces  Private
+exports.updateBootcamp = (req, res, next) => {
+  res.status(200).json({ success: true, msg: `Update a bootcamp ${req.params.id}` });
+}
+
+// Desc   Delete single bootcamp
+// Route  DELETE /api/v1/bootcamps/:id
+// Acces  Private
+exports.deleteBootcamp = (req, res, next) => {
+  res.status(200).json({ success: true, msg: `Delete a bootcamp ${req.params.id}` });
+}
