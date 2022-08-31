16d557e34653bfd9076e754975fb133c3b7727fc
raoakif <akifrao@gmail.com>
2022-08-31 06:15:14 +0500

    Add Create bootcamp controller code with try and catch async block

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -17,10 +17,18 @@ exports.getBootcamp = (req, res, next) => {
 // Desc   Create a bootcamp
 // Route  PUT /api/v1/bootcamps/:id
 // Acces  Public
-exports.createBootcamp = (req, res, next) => {
-  console.log(req.body)
-  res.status(200).json({ success: true, msg: "Create new bootcamp" });
-}
+exports.createBootcamp = async (req, res, next) => {
+  try {
+    const bootcamp = await Bootcamp.create(req.body)
+  
+    res.status(201).json({
+      success: true,
+      data: bootcamp
+    });
+  } catch (error) {
+    res.status(400).json({ success: false })
+  }
+};
 
 // Desc   Update single bootcamp
 // Route  GET /api/v1/bootcamps/:id
